a2f684d5204f26b85af5756db5ff29c1f02c6e02
Otabekova <dilafruzotabekova06@gmail.com>
2026-01-27 17:56:09 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import rasm1 from './assets/img1.jpg'
+import rasm2 from './assets/img2.jpg'
+function App() {
+
+
+  return (
+    <>
+     
+     <div>
+      <h1>Dilafruz</h1>
+       <img src={rasm1} width={200} alt="Image 1" />
+      <img src={rasm2} width={200} alt="Image 2" />
+
+     </div>
+    </>
+  )
+
+
+  
+}
+
+export default App

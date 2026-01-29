00222eb5a9a9e9f6067cad9d910ec0af4337dbe1
Otabekova <dilafruzotabekova06@gmail.com>
2026-01-29 16:55:49 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,19 @@ function App() {
   return (
     <>
      
-     <div>
-      <h1>Dilafruz</h1>
+     <div className="image-container">
+      <h1>Aston Martin Valkyrie</h1>
        <img src={rasm1} width={200} alt="Image 1" />
-      <img src={rasm2} width={200} alt="Image 2" />
+        <h4>Aston Martin Valkyrie juda chiroyli mashina</h4>
+        <h4>Aston Martin Valkyrie juda zo'r mashina</h4>
+     </div>
 
+
+     <div className="image-container">
+      <h1>Rolls-Royse</h1>
+       <img src={rasm2} width={200} alt="Image 2" />
+         <h4>Rolls-Royse juda chiroyli mashina</h4>
+        <h4>Rolls-Royse juda zo'r mashina</h4>
      </div>
     </>
   )
